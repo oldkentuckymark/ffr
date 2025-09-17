a5efbb67d3f8bf383c3b9fbecd52cef160712617
Mark Richardson <oldkentuckymark@gmail.com>
2025-09-17 03:29:52 -0400

    even more triangles work

--- ffr.hpp
+++ ffr.hpp
@@ -35,16 +35,26 @@ enum class DrawType : uint8_t
     Triangles
 };
 
+
+class VertexFunction
+{
+public:
+    VertexFunction() = default;
+    virtual ~VertexFunction() = default;
+    virtual auto operator()(const ffr::math::vec4& in) -> ffr::math::vec4 = 0;
+};
+
+
 template<uint8_t MAX_VERTS>
 class Context
 {
 public:
-    Context();
-    virtual ~Context();
+    Context() = default;
+    virtual ~Context() = default;
 
 
 
-    virtual auto plot(uint16_t x, uint16_t y, uint16_t color) -> void;
+    virtual auto plot(uint16_t x, uint16_t y, uint16_t color) -> void = 0;
 
     virtual auto line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color) -> void
     {
@@ -52,7 +62,7 @@ public:
 
         if (steep)
         {
-            int tmp = x0;
+            uint16_t tmp = x0;
             x0 = y0;
             y0 = tmp;
 
@@ -106,14 +116,42 @@ public:
         line(x0,y0,x0,y1,color);
     }
 
-    virtual auto triangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color) -> void
+    void triangle(  int16_t x0, int16_t y0,
+                  int16_t x1, int16_t y1,
+                  int16_t x2, int16_t y2,
+                  int16_t color  )  //TODO: convert to fixed32!!!!!
+    {
+        // Sort vertices by y-coordinate ascending (y0 <= y1 <= y2)
+        if (y0 > y1) { int16_t tx = x0; int16_t ty = y0; x0 = x1; y0 = y1; x1 = tx; y1 = ty; }
+        if (y1 > y2) { int16_t tx = x1; int16_t ty = y1; x1 = x2; y1 = y2; x2 = tx; y2 = ty; }
+        if (y0 > y1) { int16_t tx = x0; int16_t ty = y0; x0 = x1; y0 = y1; x1 = tx; y1 = ty; }
+
+        int16_t totalHeight = y2 - y0;
+        for (int16_t i = 0; i < totalHeight; ++i) {
+            bool secondHalf = i > y1 - y0 || y1 == y0;
+            int16_t segmentHeight = secondHalf ? y2 - y1 : y1 - y0;
+            float alpha = (float)i / totalHeight;
+            float beta  = (float)(i - (secondHalf ? y1 - y0 : 0)) / segmentHeight;
+
+            int16_t ax = x0 + (int16_t)((x2 - x0) * alpha);
+            int16_t bx = secondHalf
+                             ? x1 + (int16_t)((x2 - x1) * beta)
+                             : x0 + (int16_t)((x1 - x0) * beta);
+
+            if (ax > bx) { int16_t tmp = ax; ax = bx; bx = tmp; }
+
+            lineHorizontal(y0 + i, ax, bx, color);
+        }
+    }
+
+    virtual auto clear() -> void
     {
 
     }
+    virtual auto present() -> void
+    {
 
-
-    virtual auto clear() -> void;
-    virtual auto present() -> void;
+    }
 
     auto setVertexPointer(uint8_t size, void* vp) -> void
     {
@@ -146,13 +184,38 @@ private:
     auto vertex_pipeline() -> void;
 
 
-
-
-
-
 };
 
 
 }
 
+
+
+// void triangle(  int16_t x0, int16_t y0,
+//               int16_t x1, int16_t y1,
+//               int16_t x2, int16_t y2,
+//               int16_t color  )
+// {
+//     // Sort vertices by y-coordinate ascending (y0 <= y1 <= y2)
+//     if (y0 > y1) { int16_t tx = x0; int16_t ty = y0; x0 = x1; y0 = y1; x1 = tx; y1 = ty; }
+//     if (y1 > y2) { int16_t tx = x1; int16_t ty = y1; x1 = x2; y1 = y2; x2 = tx; y2 = ty; }
+//     if (y0 > y1) { int16_t tx = x0; int16_t ty = y0; x0 = x1; y0 = y1; x1 = tx; y1 = ty; }
+
+//     int16_t totalHeight = y2 - y0;
+//     for (int16_t i = 0; i < totalHeight; ++i) {
+//         bool secondHalf = i > y1 - y0 || y1 == y0;
+//         int16_t segmentHeight = secondHalf ? y2 - y1 : y1 - y0;
+//         float alpha = (float)i / totalHeight;
+//         float beta  = (float)(i - (secondHalf ? y1 - y0 : 0)) / segmentHeight;
+
+//         int16_t ax = x0 + (int16_t)((x2 - x0) * alpha);
+//         int16_t bx = secondHalf
+//                          ? x1 + (int16_t)((x2 - x1) * beta)
+//                          : x0 + (int16_t)((x1 - x0) * beta);
+
+//         if (ax > bx) { int16_t tmp = ax; ax = bx; bx = tmp; }
+
+//         lineHorizontal(y0 + i, ax, bx, color);
+//     }
+// }
 
--- ffrtest.cpp
+++ ffrtest.cpp
@@ -14,12 +14,48 @@ uint16_t car[6] =
 
 
 
+class SDL_Context : public ffr::Context<64>
+{
+public:
+    SDL_Context()
+    {
+        SDL_CreateWindowAndRenderer(240,160,0,&win,&ren);
+    }
+
+    ~SDL_Context()
+    {
+
+    }
+
+    void clear() override
+    {
+        SDL_SetRenderDrawColor(ren,0,0,0,255);
+        SDL_RenderClear(ren);
+    }
+
+    void present() override
+    {
+        SDL_RenderPresent(ren);
+    }
+
+    void plot(uint16_t x, uint16_t y, uint16_t c) override
+    {
+        SDL_SetRenderDrawColor(ren,255,255,255,255);
+        SDL_RenderDrawPoint(ren,x,y);
+    }
+
+private:
+    SDL_Window* win;
+    SDL_Renderer* ren;
+};
+
 
 auto main(int argc, char *argv[]) -> int
 {
 
     SDL_Init(SDL_INIT_VIDEO);
 
+    SDL_Context c;
 
     bool running = true;
     while (running)
@@ -48,7 +84,11 @@ auto main(int argc, char *argv[]) -> int
         }
 
         //const auto sintable = ffr::math::makeTable< int,30,std::sinf >;
+        c.clear();
 
+        c.triangle(20,50,100,20,100,100, 0);
+
+        c.present();
 
 
     }
