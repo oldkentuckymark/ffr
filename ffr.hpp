2090d35fc6037cb6c869b4a2682b2fa33fcf196e
Mark Richardson <oldkentuckymark@gmail.com>
2025-09-17 00:44:09 -0400

    more work

--- ffr.hpp
+++ ffr.hpp
@@ -35,6 +35,7 @@ enum class DrawType : uint8_t
     Triangles
 };
 
+template<uint8_t MAX_VERTS>
 class Context
 {
 public:
@@ -42,26 +43,107 @@ public:
     virtual ~Context();
 
 
+
     virtual auto plot(uint16_t x, uint16_t y, uint16_t color) -> void;
 
-    virtual auto line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color) -> void;
-    virtual auto lineHorizontal(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t color) -> void;
-    virtual auto lineVertical(uint16_t x0, uint16_t y0, uint16_t y1, uint16_t color) -> void;
+    virtual auto line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color) -> void
+    {
+        bool const steep = std::abs(y1 - y0) > std::abs(x1 - x0);
+
+        if (steep)
+        {
+            int tmp = x0;
+            x0 = y0;
+            y0 = tmp;
+
+            tmp = x1;
+            x1 = y1;
+            y1 = tmp;
+        }
+
+        if (x0 > x1)
+        {
+            int16_t tmp = x0;
+            x0 = x1;
+            x1 = tmp;
+
+            tmp = y0;
+            y0 = y1;
+            y1 = tmp;
+        }
+
+        int16_t const dx    = x1 - x0;
+        int16_t const dy    = std::abs(y1 - y0);
+        int16_t error = dx / 2;
+        int16_t const ystep = (y0 < y1) ? 1 : -1;
+        int16_t y     = y0;
+
+        for (int16_t x = x0; x <= x1; ++x)
+        {
+            if (steep)
+            {
+                plot(y, x, color);
+            }
+            else
+            {
+                plot(x, y, color);
+            }
+
+            error -= dy;
+            if (error < 0)
+            {
+                y     += ystep;
+                error += dx;
+            }
+        }
+    }
+    virtual auto lineHorizontal(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t color) -> void
+    {
+        line(x0,y0,x1,y0,color);
+    }
+    virtual auto lineVertical(uint16_t x0, uint16_t y0, uint16_t y1, uint16_t color) -> void
+    {
+        line(x0,y0,x0,y1,color);
+    }
+
+    virtual auto triangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color) -> void
+    {
+
+    }
+
 
     virtual auto clear() -> void;
     virtual auto present() -> void;
 
-    auto setVertexPointer(uint8_t size, uint8_t stride, void* vp) -> void;
-    auto setColorPointer(uint8_t stride, uint16_t* cp)-> void;
+    auto setVertexPointer(uint8_t size, void* vp) -> void
+    {
+        vertex_pointer_ = vp;
+        vertex_size_ = size;
+    }
+    auto setColorPointer(uint16_t* cp)-> void
+    {
+        color_pointer_ = cp;
+    }
+    auto setViewPort(uint16_t w, uint16_t h)
+    {
+        view_width_ = w;
+        view_height_ = h;
+    }
 
     auto drawArray(DrawType dt, uint8_t first, uint8_t count) -> void;
 
 private:
+    uint16_t view_width_ = 0;
+    uint16_t view_height_ = 0;
+
     void* vertex_pointer_ = nullptr;
     uint16_t* color_pointer_ = nullptr;
+    uint8_t vertex_size_ = 0;
 
-    std::vector<math::vec4> vert_buf;
-    std::vector<uint16_t> color_buf;
+    std::array<math::vec4, MAX_VERTS> vert_buf_;
+    std::array<uint16_t, (MAX_VERTS/3) + (MAX_VERTS%3)> color_buf_;
+
+    auto vertex_pipeline() -> void;
 
 
 
